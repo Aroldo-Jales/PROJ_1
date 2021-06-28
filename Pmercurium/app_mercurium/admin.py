6d3b58dc86381fb9a1e7906b4e740e077253a21d
Aroldo Jales <48775971+Aroldo-Jales@users.noreply.github.com>
2021-06-28 14:24:27 -0300

    Update admin.py

--- Pmercurium/app_mercurium/admin.py
+++ Pmercurium/app_mercurium/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Item, Category
-
-=======
 from .models import Item, Category, Wallet
 
 admin.site.register(Wallet)
->>>>>>> cf4e4c6 (wallet)
 admin.site.register(Item)
 admin.site.register(Category)
