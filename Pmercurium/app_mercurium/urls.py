8cb358cf7bf7c6f4fb39c82b8ee83ee06b8a89f5
Aroldo Jales <aroldo_jales@hotmail.com>
2021-06-30 22:28:23 -0300

    add urls

--- Pmercurium/app_mercurium/urls.py
+++ Pmercurium/app_mercurium/urls.py
@@ -3,9 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', index),
+    path('wallets.html', wallets),
     path('create.html', create),
     path('update/<int:id>', update),
     path('trash.html', trash),
+    path('delete/<int:id>', delete),
     path('permanentlydelete/<int:id>', permanently_delete),
     path('recycle/<int:id>', recycle),
     path('pay/<int:id>', pay),
