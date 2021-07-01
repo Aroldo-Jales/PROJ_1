363d2f81712718626cfe8bfce95a0ce8531d858d
Aroldo Jales <aroldo_jales@hotmail.com>
2021-06-30 22:27:21 -0300

    modal filter

--- Pmercurium/app_mercurium/static/js/app.js
+++ Pmercurium/app_mercurium/static/js/app.js
@@ -1,7 +1,15 @@
-let msg = document.querySelector('.messages');
-let close = document.querySelector('.close-message');
+function modal_filter() {
+    let modal_bg = document.querySelector('.modal-bg');
+    modal_bg.classList.add('bg-active');
+}
+
+function close_modal_filter() {
+    let modal_bg = document.querySelector('.modal-bg');
+    modal_bg.classList.remove('bg-active');
+}
 
 function close_message() {
+    let msg = document.querySelector('.messages');
     msg.style.display = 'none';
 }
 
