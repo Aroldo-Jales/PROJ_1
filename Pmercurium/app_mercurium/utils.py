8ccb0df3dfbe2865fd784f8f3e51566f2738f945
Aroldo Jales <aroldo_jales@hotmail.com>
2021-07-01 21:33:14 -0300

    create def filter

--- Pmercurium/app_mercurium/utils.py
+++ Pmercurium/app_mercurium/utils.py
@@ -1,6 +1,55 @@
 from datetime import date
+from django.contrib import messages
 from django.db.models import Sum
+from .models import *
 
 def days_between_dates(date):
     today = date.today()
     return abs((today-date).days)
+
+def filter_items(request, items, word_key=None, init_date=None, end_date=None, init_value=None, end_value=None):
+
+    if (word_key):
+        items = items.filter(description__contains=word_key)
+
+    if (init_date and end_date):
+        if init_date <= end_date:
+            items = items.filter(date__range=[init_date, end_date])
+        else:
+            message_error(request,'date-range')
+    else:    
+        if (init_date):
+            message_error(request,'incomplete-date-2')
+        elif (end_date):
+            message_error(request,'incomplete-date-1')
+
+    if (init_value and end_value):
+        init_value = float(init_value)
+        end_value = float(end_value)
+        if init_value <= end_value:
+            items = items.filter(value__range=[init_value, end_value])
+        else:
+            message_error(request,'value-range')
+    else:    
+        if (init_value):
+            message_error(request,'incomplete-value-2')
+        elif (end_value):
+            message_error(request,'incomplete-value-1')
+    return items    
+
+def message_error(request, invalid_type):
+    messages_invalid = {
+        'date-range': "A data inicial é maior que a final!",
+        'incomplete-date-1': "Preencha a data inicial!",
+        'incomplete-date-2': "Preencha a data final!",
+        'value-range': "O valor inicial é maior que o final!",
+        'incomplete-value-1': "Preencha o valor inicial!",
+        'incomplete-value-2': "Preencha o valor final!",
+        'delete': "Registro movido para a lixeira!",
+        'permanently_delete': "Registro removido permanetimente!",
+        'date_form': "A data de vencimento é menor que a data inicial",
+        'delete_cat': "Categoria removida",
+        'not_delete' : "A categoria está ligada a outros registros"
+    }
+
+    return messages.error(request, messages_invalid.get(invalid_type))
--- Pmercurium/app_mercurium/views.py
+++ Pmercurium/app_mercurium/views.py
@@ -19,50 +19,21 @@ def index(request):
         word_key = request.GET.get('word-key')
 
         init_date = request.GET.get('init-date')
-        end_date = request.GET.get('end-date')
+        end_date = request.GET.get('end-date')      
 
         init_value = request.GET.get('init-value')
         end_value = request.GET.get('end-value')
 
-        if (word_key):
-            items = items.filter(description__contains=word_key)
-
-        if (init_date and end_date):
-            if init_date <= end_date:
-                items = items.filter(date__range=[init_date, end_date])
-            else:
-                message_error(request,'date-range')
-
-        else:    
-            if (init_date):
-                message_error(request,'incomplete-date-2')
-            elif (end_date):
-                message_error(request,'incomplete-date-1')
-
-        if (init_value and end_value):
-            init_value = float(init_value)
-            end_value = float(end_value)
-            if init_value <= end_value:
-                items = items.filter(value__range=[init_value, end_value])
-            else:
-                message_error(request,'value-range')
-        else:    
-            if (init_value):
-                message_error(request,'incomplete-value-2')
-            elif (end_value):
-                message_error(request,'incomplete-value-1')
-    
+        items = filter_items(request, items, word_key, init_date, end_date, init_value, end_value)
+        
     gain = items.filter(type_item='Receita', status_payment=True).aggregate(Sum('value'))
     to_gain = items.filter(type_item='Receita', status_payment=False).aggregate(Sum('value'))
-    
     expenses = items.filter(type_item='Despesa', status_payment=True).aggregate(Sum('value'))
     to_pay = items.filter(type_item='Despesa', status_payment=False).aggregate(Sum('value'))
-    
     current_balance = items.filter(status_payment=True).aggregate(Sum('value'))
-    
     balance = items.aggregate(Sum('value'))
     
-    paginator = Paginator(items, 4)
+    paginator = Paginator(items, 8)
     page = request.GET.get('page')
     items_list = paginator.get_page(page)
     
@@ -81,24 +52,24 @@ def create(request):
             if item.type_item  == 'Despesa' and item.value > 0 or item.type_item  == 'Receita' and item.value < 0:
                 item.value = item.value * (-1)
                 item.original_value = item.value
-                messages.success(request, 'Registro Salvo', extra_tags='safe')
                 item.save()
+                message_sucess(request, 'save', item.description)
             
                 if item.status_payment == True:
                     item.date_payment = item.date
                     item.fees = 0
-                    messages.success(request, 'Registro Salvo', extra_tags='safe')
                     item.save()
+                    message_sucess(request, 'save', item.description)
             else:
                 if item.status_payment == True:
                     item.date_payment = item.date
                     item.fees = 0
-                    messages.success(request, 'Registro Salvo', extra_tags='safe')
                     item.save()
+                    message_sucess(request, 'save', item.description)
                 else:
                     item.original_value = item.value
-                    messages.success(request, 'Registro Salvo', extra_tags='safe')
                     item.save()
+                    message_sucess(request, 'save', item.description)
 
             return redirect('/')
     else:
@@ -172,10 +143,10 @@ def delete_cat(request, id):
     form = CategoryForm()
     data = {'categories' : categories, 'form' : form}
 
-    not_delete = cat.is_deletable()
+    related_list = cat.is_deletable()
     
-    if not_delete:
-        message_error(request, 'not_delete')
+    if related_list:
+        message_error(request, 'not_delete', cat.name)
         return render(request, 'categories.html', data)
     
     else:
@@ -185,13 +156,25 @@ def delete_cat(request, id):
 
 def trash(request):
     items = Item.objects.filter(status=False)
+
+    if request.method == 'GET':
+        word_key = request.GET.get('word-key')
+
+        init_date = request.GET.get('init-date')
+        end_date = request.GET.get('end-date')      
+
+        init_value = request.GET.get('init-value')
+        end_value = request.GET.get('end-value')
+
+        items = filter_items(request, items, word_key, init_date, end_date, init_value, end_value)
+
     data = {'items' : items}
     return render(request, 'trash.html', data)
 
 def permanently_delete(request, id):
     item = get_object_or_404(Item, pk=id)
     item.delete()
-    message_error(request,'permanently_delete')
+    message_error(request, 'permanently_delete')
 
     return redirect('/')
 
@@ -200,7 +183,7 @@ def recycle(request, id):
 
     item.status = True
     item.save()
-    messages.success(request, 'Registro recuperado!', extra_tags='safe')
+    message_sucess(request, 'recycle', item.description)
 
     return redirect('/')
 
@@ -223,7 +206,15 @@ def calc_fee(items):
             item.value = item.original_value + (item.original_value * ((item.fees/100)*days))
             item.save()
 
-def message_error(request, invalid_type):
+def message_sucess(request, sucess_type, name=''):
+    messages_sucess = {
+        'save': f"Registro {name} salvo!",
+        'recycle' : f"Registro {name} recuperado!"
+    }
+
+    return messages.success(request, messages_sucess.get(sucess_type), extra_tags='safe')
+
+def message_error(request, invalid_type, name=''):
     messages_invalid = {
         'date-range': "A data inicial é maior que a final!",
         'incomplete-date-1': "Preencha a data inicial!",
@@ -231,11 +222,11 @@ def message_error(request, invalid_type):
         'value-range': "O valor inicial é maior que o final!",
         'incomplete-value-1': "Preencha o valor inicial!",
         'incomplete-value-2': "Preencha o valor final!",
-        'delete': "Registro movido para a lixeira!",
-        'permanently_delete': "Registro removido permanetimente!",
+        'delete': f"Registro {name} movido para a lixeira!",
+        'permanently_delete': f"Registro {name} removido permanetimente!",
         'date_form': "A data de vencimento é menor que a data inicial",
-        'delete_cat': "Categoria removida",
-        'not_delete' : "A categoria está ligada a outros registros"
+        'delete_cat': f"Categoria {name} removida",
+        'not_delete' : f"A categoria {name} está ligada a outros registros"
     }
-    return messages.error(request, messages_invalid.get(invalid_type))
 
+    return messages.error(request, messages_invalid.get(invalid_type))
