268f11e30638dbe076b6a2a6b8daf9e08399aa40
Aroldo Jales <aroldo_jales@hotmail.com>
2021-07-08 22:25:56 -0300

    hide and show input form

--- Pmercurium/app_mercurium/static/js/app.js
+++ Pmercurium/app_mercurium/static/js/app.js
@@ -13,3 +13,16 @@ function close_message() {
     msg.style.display = 'none';
 }
 
+let status_check = document.getElementById('id_status_payment');
+document.getElementById('id_date_payment').style.display = 'none';
+document.getElementById('id_fees').style.display = 'none';
+
+status_check.onclick = () => {
+    if (status_check.checked == true) {
+        document.getElementById('id_date_payment').style.display = 'none';
+        document.getElementById('id_fees').style.display = 'none';
+    } else {
+        document.getElementById('id_date_payment').style.display = 'block';
+        document.getElementById('id_fees').style.display = 'block';
+    }
+};
